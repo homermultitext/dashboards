00e57fe758b143bf240c48e30130d44b5b1ba3b1
Neel Smith <dnsmith.neel@gmail.com>
2022-02-20 02:51:19 -0500

    try some plotting

--- thermometer/thermometer.jl
+++ thermometer/thermometer.jl
@@ -63,3 +63,56 @@ app.layout = html_div() do
 end
 
 run_server(app, "0.0.0.0", DEFAULT_PORT, debug=true)
+
+
+#=
+url = "https://raw.githubusercontent.com/homermultitext/hmt-archive/master/releases-cex/hmt-current.cex"
+
+using CitableBase, CitableObject, CitableImage, CitableText
+using CitablePhysicalText
+using CitableAnnotations
+using CiteEXchange
+using Downloads
+
+
+function loadhmtdata(url::AbstractString)
+    cexfile = Downloads.download(url)
+    cexsrc = read(cexfile) |> String
+    codexlist = fromcex(cexsrc, Codex)
+    indexing = fromcex(cexsrc, TextOnPage)
+    libinfo = blocks(cexsrc, "citelibrary")[1]
+    infoparts = split(libinfo.lines[1], "|")
+    (codexlist, indexing, infoparts[2])
+end
+
+dataurl = "https://raw.githubusercontent.com/homermultitext/hmt-archive/master/releases-cex/hmt-current.cex"
+(codices, indexes, releaseinfo) = loadhmtdata(dataurl)
+
+data = []
+for idx in indexes
+    for pr in idx.data
+        txt = pr[1]
+        bk = collapsePassageBy(txt, 1) |> passagecomponent
+        pieces = split(workcomponent(txt), ".")
+        push!(data, (pieces[end], bk))
+    end
+end
+
+using SplitApplyCombine
+grp = group(data)
+
+counts = []
+grp = group(data)
+counts = []
+for k in keys(grp)
+	push!(counts, (ms = k[1], bk = k[2], count = length(grp[k])))
+end
+
+mss = map(trpl -> trpl[1], counts) |> unique
+
+dataseries = Dict()
+for ms in mss
+
+end
+
+=#
