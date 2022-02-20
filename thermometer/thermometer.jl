5b0a0a1d31c5cac6d440f1a3cb1d8f6468e753c8
Neel Smith <dnsmith.neel@gmail.com>
2022-02-20 07:59:08 -0500

    Rm plot packages other than PlotlyJS

--- thermometer/thermometer.jl
+++ thermometer/thermometer.jl
@@ -34,8 +34,7 @@ using Downloads
 using Unicode
 using SplitApplyCombine
 using Tables
-using Plots
-
+using PlotlyJS
 
 """ Extract text catalog, normalized editions of texts,
 and release info from HMT publication.
@@ -76,8 +75,46 @@ function textlist(textcatalog)
 end
 
 function indexgraph(indices)
-    ""
+    data = []
+    for idx in indexes
+        for pr in idx.data
+            txt = pr[1]
+            bk = collapsePassageBy(txt, 1) |> passagecomponent
+            pieces = split(workcomponent(txt), ".")
+            push!(data, (pieces[end], bk))
+        end
+    end
+
+    grp = group(data)
+    counts = []
+    grp = group(data)
+    counts = []
+    for k in keys(grp)
+        push!(counts, (ms = k[1], bk = k[2], count = length(grp[k])))
+    end
+
+
+    mss = map(trpl -> trpl[1], counts) |> unique
+    bks = collect(1:24) .|> string
+
+    dataseries = Dict()
+    for ms in mss   
+        padded = []
+        for bk in bks
+            extract = filter(trp -> trp[1] == ms && trp[2] == bk, counts)
+            isempty(extract) ? push!(padded, (ms = ms,bk = bk,count = 0)) : push!(padded, extract[1])
+        end
+        dataseries[ms] = Tables.columntable(padded)
+    end
+
+    barlist = GenericTrace{Dict{Symbol, Any}}[]
+    for k in keys(dataseries)
+        tbl = dataseries[k]
+        push!(barlist, bar(name=k, x=tbl.bk, y=tbl.count))
+    end
+    Plot(barlist)
 end
+
 app = if haskey(ENV, "URLBASE")
     dash(assets_folder = assets, url_base_pathname = ENV["URLBASE"])
 else 
@@ -149,27 +186,7 @@ run_server(app, "0.0.0.0", DEFAULT_PORT, debug=true)
 
 
 #=
-url = "https://raw.githubusercontent.com/homermultitext/hmt-archive/master/releases-cex/hmt-current.cex"
 
-using CitableBase, CitableObject, CitableImage, CitableText
-using CitablePhysicalText
-using CitableAnnotations
-using CiteEXchange
-using Downloads
-
-
-function loadhmtdata(url::AbstractString)
-    cexfile = Downloads.download(url)
-    cexsrc = read(cexfile) |> String
-    codexlist = fromcex(cexsrc, Codex)
-    indexing = fromcex(cexsrc, TextOnPage)
-    libinfo = blocks(cexsrc, "citelibrary")[1]
-    infoparts = split(libinfo.lines[1], "|")
-    (codexlist, indexing, infoparts[2])
-end
-
-dataurl = "https://raw.githubusercontent.com/homermultitext/hmt-archive/master/releases-cex/hmt-current.cex"
-(codices, indexes, releaseinfo) = loadhmtdata(dataurl)
 
 data = []
 for idx in indexes
@@ -181,9 +198,7 @@ for idx in indexes
     end
 end
 
-using SplitApplyCombine
 grp = group(data)
-
 counts = []
 grp = group(data)
 counts = []
