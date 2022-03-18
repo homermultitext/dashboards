6f20c3e018c1a4eb9e439c213d2c882ae093de70
Neel Smith <dnsmith.neel@gmail.com>
2022-03-18 03:38:46 -0400

    Begin rework of thermometer using new `HmtArchive.Analysis` module

---
+++ thermometer/therm2.jl
@@ -0,0 +1,139 @@
+# Run this dashboard from the root of the
+# github repository:
+using Pkg
+Pkg.activate(joinpath(pwd(), "thermometer"))
+Pkg.instantiate()
+
+DASHBOARD_VERSION = "0.4.0"
+
+# Variables configuring the app:  
+#
+#  1. location  of the assets folder (CSS, etc.)
+#  2. port to run on
+# 
+# Set an explicit path to the `assets` folder
+# on the assumption that the dashboard will be started
+# from the root of the gh repository!
+assets = joinpath(pwd(), "thermometer", "assets")
+DEFAULT_PORT = 8060
+using Dash
+
+using CitableAnnotations
+using CitableBase
+using CitableCorpus
+using CitableImage
+using CitableObject
+using CitablePhysicalText
+using CitableText
+using CiteEXchange
+using HmtArchive
+using HmtArchive.Analysis
+
+using Downloads
+using SplitApplyCombine
+using Tables
+using PlotlyJS
+
+
+src = hmt_cex()
+
+""" Extract text catalog, normalized editions of texts,
+and release info from HMT publication.
+"""
+function loadhmtdata()
+    cexsrc = hmt_cex()
+
+    allimgs = hmt_images(cexsrc)
+    imgs = filter(c -> length(c) > 2, allimgs)
+
+    mss = hmt_codices(cexsrc)
+    indexing = hmt_pageindex(cexsrc)
+    ctscatalog = hmt_textcatalog(cexsrc)
+    normalizedtexts = hmt_normalized(cexsrc)
+
+    (imgs, mss, indexing, ctscatalog, normalizedtexts, hmt_releaseinfo(cexsrc))
+end
+
+(images, codices, indexes, textcatalog, normalizededition, releaseinfo) = loadhmtdata()
+
+
+intro = """
+
+**Current release**:  The complete contents of the HMT project's archive are published in plain-text files in the CITE EXchange format (CEX). The current published release  is always available in a file named `hmt-current.cex` in the project's archival github repository, in the `archive/releases-cex`  directory. (Here is a link to [the raw CEX file](https://raw.githubusercontent.com/homermultitext/hmt-archive/master/releases-cex/hmt-current.cex).)
+
+**Earlier releases**: Earlier releases are available in subdirectories of `archive/releases-cex` by year.  In 2018, the HMT project published 5 releases; in 2020, 9 releases.
+
+Releases in 2022 are sequentially named `hmt-2022`**ID**`.cex` where **ID** is a successive letter of the alphabet.
+
+"""
+
+function sums(imgcollections, mslist, normededitions, textcat)
+    imgcount = length.(imgcollections) |> sum
+    pagecount = map(ms -> length(ms), mslist) |> sum
+    mscount = length(mslist)
+
+    iliadlines = filter(psg -> startswith(workcomponent(psg.urn), "tlg0012.tlg001"), normededitions.passages) |> length
+
+
+    scholia = filter(psg -> startswith(workcomponent(psg.urn), "tlg5026"), normededitions.passages)
+    commentcount = filter(psg-> endswith(passagecomponent(psg.urn), "comment"),  scholia) |> length
+    wordcount = map(psg -> split(psg.text) |> length, normalizededition) |> sum
+    doccount = length(textcat)
+    """## Summary
+The current release of the HMT archive publishes:
+  
+
+> - **$(imgcount)** cataloged images in **$(length(images)) collections**
+> - **$(pagecount) pages** in **$(mscount) manuscripts**
+> - **$(wordcount) words** in diplomatic editions of **$(doccount) cataloged documents**
+> - diplomatic and normalized editions of **$(iliadlines) lines** of the *Iliad*
+> - diplomatic and normalized editions of **$(commentcount) scholia**
+
+"""    
+end
+
+app = if haskey(ENV, "URLBASE")
+    dash(assets_folder = assets, url_base_pathname = ENV["URLBASE"])
+else 
+    dash(assets_folder = assets)    
+end
+
+app.layout = html_div() do
+    dcc_markdown("*Dashboard version*: **$(DASHBOARD_VERSION)**"),
+
+
+    html_h1("$(releaseinfo)"),
+    
+   
+    dcc_markdown(intro),
+
+    dcc_markdown(
+        sums(images, codices, normalizededition, textcatalog)
+    ),
+
+    html_div(className = "panel",
+        children = [
+            html_div(
+                className = "columnl",
+                children = [
+                    dcc_markdown("## Images")
+                ]
+            ),
+            html_div(
+                className = "columnr",
+                children = [
+                    dcc_markdown("""## Manuscripts
+
+**$(length(codices))** cataloged manuscripts
+                
+Explore manuscripts with the [codex-browser dashboard](https://www.homermultitext.org/codex-browser/).
+"""
+                    )
+                   
+                ]
+            )
+        ]
+    )
+end
+
+run_server(app, "0.0.0.0", DEFAULT_PORT, debug=true)
